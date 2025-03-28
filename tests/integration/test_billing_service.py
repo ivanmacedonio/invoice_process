c452bb6c06a498230e3d42bc93e355bcdf43c14b
ivandevclub <imacedonio@sportclub.team>
2025-03-28 15:15:01 -0300

    hotfix: integration tests

--- tests/integration/test_billing_service.py
+++ tests/integration/test_billing_service.py
@@ -30,6 +30,6 @@ class TestBillingService(TestCase):
             transaction=dummy_transaction
         )
 
-        self.assertTrue('CAE' in arca_response['message'])
-        self.assertTrue('CAEFchVto' in arca_response['message'])
-        self.assertTrue(isinstance(arca_response['message'], dict))
+        self.assertTrue('CAE' in arca_response['cae_data'])
+        self.assertTrue('CAEFchVto' in arca_response['cae_data'])
+        self.assertTrue(isinstance(arca_response['cae_data'], dict))
